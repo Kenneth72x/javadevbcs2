93abc6e61970f32dc29cbacaf9dba57373e86cc9
Kenneth Rothstein <skottrod@me.com>
2022-10-08 09:14:43 -0700

    Modulus operator example complete

--- MathOperations.java
+++ MathOperations.java
@@ -24,8 +24,8 @@ public class MathOperations {
         System.out.println("-Andromeda has " + (starsInAndromeda / starsInMilkyWay) + " times more stars.");
         System.out.println("-Snape caught the error. Furious, he divided Ron's score to: " + (testScore / bonusMarks)+ ".");
 
-        System.out.println("10 is an even number, since dividing 10 by 2 has a remainder of: <remainder>");
-        System.out.println("5 is an odd number, since dividing 5 by 2 has a remainder of: <remainder>");
+        System.out.println("-10 is an even number, since dividing 10 by 2 has a remainder of: " + (10 % 2 ));
+        System.out.println("-5 is an odd number, since dividing 5 by 2 has a remainder of: " + (5 % 2));
 
     }
     
