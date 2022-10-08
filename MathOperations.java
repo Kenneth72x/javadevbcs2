1e209d258a97afc325723b11bbacfebe0bd9b602
Kenneth Rothstein <skottrod@me.com>
2022-10-08 09:32:14 -0700

    Added counter++ example

--- MathOperations.java
+++ MathOperations.java
@@ -27,6 +27,12 @@ public class MathOperations {
         System.out.println("-10 is an even number, since dividing 10 by 2 has a remainder of: " + (10 % 2 ));
         System.out.println("-5 is an odd number, since dividing 5 by 2 has a remainder of: " + (5 % 2));
 
+        int counter = 0;
+        counter++;
+        counter++;
+        counter++;
+        System.out.println("-I count: " + counter);
+
     }
     
 }
