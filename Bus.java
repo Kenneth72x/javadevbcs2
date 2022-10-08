46f9b35c86bdea5b8ea21a790cabc5190a28c375
Kenneth Rothstein <skottrod@me.com>
2022-10-08 08:46:26 -0700

    completed lesson

---
+++ Bus.java
@@ -0,0 +1,13 @@
+public class Bus {
+    public static void main(String[] args){
+        String sentence = "Number of Passengers: ";
+        int passengers = 0;
+        passengers += 9;
+        System.out.println(sentence + passengers);
+        passengers -= 5;
+        System.out.println(sentence + passengers);
+        passengers -= 4;
+        System.out.println(sentence + passengers);
+        System.out.println("End of Shift! The Ministry of Transportation thanks you Kenneth!");
+    }
+}
---
+++ Form.java
@@ -0,0 +1,9 @@
+public class Form {
+    public static void main(String[] args){
+        char gender = 'f';
+        String name = "Jenna";
+        System.out.println("Gender: " + gender);
+        System.out.println("Name: " + name);
+    }
+    
+}
